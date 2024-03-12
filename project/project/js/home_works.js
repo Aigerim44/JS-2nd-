89f785f501cac625f20c2cc388313f2c5e75ed70
Aigerim <156662949+Aigerim44@users.noreply.github.com>
2024-03-13 01:19:10 +0600

    Add files via upload

---
+++ project/project/js/home_works.js
@@ -0,0 +1,91 @@
+//GMAIL BLOCK---------------------------------------------------------------------------------
+const gmailInput = document.querySelector('#gmail_input')
+const gmailButton = document.querySelector('#gmail_button')
+const gmailResult = document.querySelector('#gmail_result')
+
+const regExp = /\w@gmail.com$/
+
+gmailButton.onclick = () => {
+    if (regExp.test(gmailInput.value)){
+        gmailResult.innerHTML = 'OK'
+        gmailResult.style.color = 'green'
+    }else{
+        gmailResult.innerHTML = 'NOT OK'
+        gmailResult.style.color = 'red'
+    }
+}
+
+//MOVE BLOCK 1----------------------------------------------------------------------------------
+// const childBlock = document.querySelector('.child_block')
+// const parentBlock = document.querySelector('.parent_block')
+// let position = 0
+//
+// const movingBlock = () => {
+//     if (position < 449){
+//         position++
+//         childBlock.style.left = `${position}px`
+//         requestAnimationFrame(movingBlock)
+//     }
+// }
+//
+// movingBlock()
+
+
+//MOVE BLOCK 2-----------------------------------------------------------------------------------
+const childBlock = document.querySelector('.child_block')
+const parentBlock = document.querySelector('.parent_block')
+
+let positionX = 0
+let positionY = 0
+const maxOffsetWidth = parentBlock.offsetWidth - childBlock.offsetWidth
+const maxOffsetHeight = parentBlock.offsetHeight - childBlock.offsetHeight
+
+const moveBlock = () => {
+    if (positionX < maxOffsetWidth && positionY === 0){
+       positionX++
+       childBlock.style.left = `${positionX}px`
+        // requestAnimationFrame(moveBlock)
+        setTimeout(moveBlock, 1)
+    }else if (positionX >= maxOffsetWidth && positionY < maxOffsetHeight) {
+        positionY++
+        childBlock.style.top = `${positionY}px`
+        // requestAnimationFrame(moveBlock)
+        setTimeout(moveBlock, 1)
+    }else if (positionX >= 0 && positionY <= maxOffsetHeight) {
+        positionX--
+        childBlock.style.left = `${positionX}px`
+        setTimeout(moveBlock, 1)
+    }else if (positionX <= 0 && positionY > 0) {
+        positionY--
+        childBlock.style.top = `${positionY}px`
+        setTimeout(moveBlock, 1)
+    }
+}
+moveBlock()
+
+
+//STOPWATCH----------------------------------------------------------------------------------
+const startButton = document.querySelector('#start')
+const stopButton = document.querySelector('#stop')
+const resetButton = document.querySelector('#reset')
+const seconds = document.querySelector('#seconds')
+let second = 0
+let interval
+
+const stopwatch =() => {
+    second++
+    seconds.innerHTML = `${second}`
+}
+
+startButton.onclick = () => {
+    clearInterval(interval)
+    interval = setInterval(stopwatch, 1000)
+}
+stopButton.onclick = () => {
+    clearInterval(interval)
+}
+resetButton.onclick = () => {
+    clearInterval(interval)
+    second = 0
+    seconds.innerHTML = '0'
+}
