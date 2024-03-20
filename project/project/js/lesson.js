85e7927ebda19a3f629f234041af759acd585d41
Aigerim <156662949+Aigerim44@users.noreply.github.com>
2024-03-20 14:11:29 +0600

    Add files via upload

--- project/project/js/lesson.js
+++ project/project/js/lesson.js
@@ -14,3 +14,45 @@ phoneButton.addEventListener('click', () => {
         phoneSpan.style.color = 'red'
     }
 })
+
+//TAB SLIDER-----------------------------------------------------------------------------
+const tabContent = document.querySelectorAll('.tab_content_block')
+const tabs = document.querySelectorAll('.tab_content_item')
+const tabsParent = document.querySelector('.tab_content_items')
+let index = 0
+
+const hideTabContent = () => {
+    tabContent.forEach((content) => {
+        content.style.display = 'none'
+    })
+    tabs.forEach((tab) => {
+        tab.classList.remove('tab_content_item_active')
+    })
+}
+
+const showTabContent = (index = 0) => {
+    tabContent[index].style.display = 'block'
+    tabs[index].classList.add('tab_content_item_active')
+}
+hideTabContent()
+showTabContent(index)
+
+tabsParent.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')){
+        tabs.forEach((tab, tabIndex) => {
+            if (event.target === tab) {
+                hideTabContent()
+                index = tabIndex
+                showTabContent(tabIndex)
+            }
+        })
+    }
+}
+
+const autoSlider = () => {
+    hideTabContent()
+    index = (index + 1) % tabs.length
+    showTabContent(index)
+}
+
+setInterval(autoSlider, 3000)
