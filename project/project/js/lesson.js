8f725153d1000a361bd5ff899b58649f66e947fe
Aigerim <156662949+Aigerim44@users.noreply.github.com>
2024-03-23 02:01:15 +0600

    Add files via upload

--- project/project/js/lesson.js
+++ project/project/js/lesson.js
@@ -43,7 +43,7 @@ tabsParent.onclick = (event) => {
             if (event.target === tab) {
                 hideTabContent()
                 index = tabIndex
-                showTabContent(tabIndex)
+                showTabContent(index)
             }
         })
     }
@@ -56,3 +56,93 @@ const autoSlider = () => {
 }
 
 setInterval(autoSlider, 3000)
+
+
+//CONVERTER-----------------------------------------------------------------------------------------
+
+const somInput = document.querySelector('#som')
+const usdInput = document.querySelector('#usd')
+const eurInput = document.querySelector('#eur')
+const rubInput = document.querySelector('#rub')
+
+// somInput.addEventListener('input', () => {
+//     const request = new XMLHttpRequest()
+//     request.open('GET', '../data/converter.json')
+//     request.setRequestHeader('Content-type', 'application/json' )
+//     request.send()
+//
+//     request.onload = () => {
+//         const data = JSON.parse(request.response)
+//         usdInput.value = (somInput.value / data.usd).toFixed(2)
+//     }
+// })
+
+const converter = (element, targetElement, secondElement, thirdElement, current) => {
+    element.oninput = () => {
+        const request = new XMLHttpRequest()
+        request.open('GET', '../data/converter.json')
+        request.setRequestHeader('Content-type', 'application/json' )
+        request.send()
+
+        request.onload = () => {
+            const data = JSON.parse(request.response)
+
+            switch (current) {
+                case 'som':
+                    targetElement.value = (element.value / data.usd).toFixed(2)
+                    secondElement.value = (element.value / data.eur).toFixed(2)
+                    thirdElement.value = (element.value / data.rub).toFixed(2)
+                    break
+                case 'usd':
+                    targetElement.value = (element.value * data.usd).toFixed(2)
+                    secondElement.value = (element.value * 0.91).toFixed(2)
+                    thirdElement.value = (element.value * 92.29).toFixed(2)
+                    break
+                case 'eur':
+                    targetElement.value = (element.value * data.eur).toFixed(2)
+                    secondElement.value = (element.value * 1.01).toFixed(2)
+                    thirdElement.value = (element.value * 99.72).toFixed(2)
+                    break
+                case 'rub':
+                    targetElement.value = (element.value * data.rub).toFixed(2)
+                    secondElement.value = (element.value * 0.011).toFixed(2)
+                    thirdElement.value = (element.value * 0.011).toFixed(2)
+                    break
+                default:
+                    break
+            }
+            if (element.value === '') {
+                targetElement.value = ''
+                secondElement.value = ''
+                thirdElement.value = ''
+            }
+        }
+    }
+}
+converter(somInput, usdInput, eurInput, rubInput,'som')
+converter(usdInput, somInput, eurInput, rubInput,'usd')
+converter(eurInput, somInput, usdInput, rubInput, 'eur')
+converter(rubInput, somInput, eurInput, usdInput, 'rub')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
